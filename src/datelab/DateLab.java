979fce230b0dae11ca747c3598d10a79532b9bbe
jbaldwin2 <jbaldwin2@L11113038616.wctc.msft>
2012-03-14 18:49:04 -0500

    Initial Commit.

---
+++ src/datelab/DateLab.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datelab;
+
+/**
+ *
+ * @author jbaldwin2
+ */
+public class DateLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
